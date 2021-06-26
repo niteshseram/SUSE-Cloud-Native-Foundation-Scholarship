6c70b61db0b6ed814a685e412d88abf90cffcdbf
S Nitesh Singh <niteshseram@gmail.com>
2021-06-26 10:14:31 +0530

    adding status and metrics end points

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
 from flask import Flask
+from flask import json
 
 app = Flask(__name__)
+
+
+@app.route("/status")
+def healthcheck():
+    response = app.response_class(response=json.dumps(
+        {"result": "OK - healthy"}),
+        status=200,
+        mimetype='application/json')
+
+    return response
+
+
+@app.route("/metrics")
+def metrics():
+    response = app.response_class(response=json.dumps({
+        "status": "success",
+        "code": 0,
+        "data": {
+            "UserCount": 140,
+            "UserCountActive": 23
+        }
+    }),
+        status=200,
+        mimetype='application/json')
+
+    return response
 
 
 @app.route("/")
